a551c8f7d072d4b46e8c76120e8dbdcf8d31b929
Sai Nikhit Gulla <nikhit.gulla@mutualmobile.com>
2022-09-05 18:10:11 -0700

    Remove iOS Frontend

--- Mc_Project_1_Frontend/Mc_Project_1_Frontend/CategoriesView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  CategoriesView.swift
-//  Mc_Project_1_Frontend
-//
-//  Created by Sai Nikhit Gulla on 8/28/22.
-//
-
-import Foundation
--- Mc_Project_1_Frontend/Mc_Project_1_Frontend/NetworkManager.swift
+++
@@ -1,8 +0,0 @@
-//
-//  NetworkManager.swift
-//  Mc_Project_1_Frontend
-//
-//  Created by Sai Nikhit Gulla on 8/27/22.
-//
-
-import Foundation
